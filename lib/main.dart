7cc70011b7355a3ba9545edac2392e736f67905a
etorunski <etorunski@gmail.com>
2024-09-12 14:42:36 -0400

    Adding Widgets

--- lib/main.dart
+++ lib/main.dart
@@ -105,13 +105,11 @@ class _MyHomePageState extends State<MyHomePage> {
           // wireframe for each widget.
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
+            const Text('You have pushed the button this many times:',style:TextStyle(fontSize: 60.0)),
+
+            Image.asset("images/algonquin.jpg", height:600, width:800)
+
+
           ],
         ),
       ),
