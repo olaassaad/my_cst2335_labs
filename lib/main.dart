4b05ca4cfeccabd6d3dca5265f8a2103c920ef5c
Ola Assaad <assa0103@algonquinlive.com>
2025-01-27 20:01:03 -0500

    Lab Week 3

--- lib/main.dart
+++ lib/main.dart
@@ -5,172 +5,120 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text(
+            'BROWSE CATEGORIES',
+            style: TextStyle(fontWeight: FontWeight.bold),
+          ),
+          centerTitle: true,
+        ),
+        body: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              const Text(
+                'Not sure about exactly which recipe you are looking for? Do a search, or dive into our most popular categories.',
+                textAlign: TextAlign.left,
+                style: TextStyle(fontSize: 18),
+              ),
+              const Text(
+                'BY MEAT',
+                textAlign: TextAlign.center,
+                style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+              ),
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
+              // Row 1: Images with text in the center
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  buildImageStack('images/Beef.jpg', 'BEEF'),
+                  buildImageStack('images/Chicken.jpg', 'CHICKEN'),
+                  buildImageStack('images/Pork.jpg', 'PORK'),
+                  buildImageStack('images/Seafood.jpg', 'SEAFOOD'),
+                ],
+              ),
+              const Text(
+                'BY COURSE',
+                textAlign: TextAlign.center,
+                style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+              ),
+              // Row 2: Images with text at the bottom
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  buildImageWithTextBelow('images/Main.jpg', 'Main Dishes'),
+                  buildImageWithTextBelow('images/Salad.jpg', 'Salad Recipes'),
+                  buildImageWithTextBelow('images/Side.jpg', 'Side Dishes'),
+                  buildImageWithTextBelow('images/Crockpot.jpg', 'Crockpot'),
+                ],
+              ),
+              const Text(
+                'BY DESSERT',
+                textAlign: TextAlign.center,
+                style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+              ),
+              // Row 3: Dessert images with text at the bottom
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  buildImageWithTextBelow('images/IceCream.jpg', 'Ice Cream'),
+                  buildImageWithTextBelow('images/Brownies.jpg', 'Brownies'),
+                  buildImageWithTextBelow('images/Pie.jpg', 'Pies'),
+                  buildImageWithTextBelow('images/Cookies.jpg', 'Cookies'),
+                ],
+              ),
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  late TextEditingController _controller; //late - Constructor in initState()
-
-  var isChecked = false;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-
-  @override //same as in java
-  void initState() {
-    super.initState(); //call the parent initState()
-    _controller = TextEditingController(); //our late constructor
-  }
-
-
-  @override
-  void dispose()
-  {
-    super.dispose();
-    _controller.dispose();    // clean up memory
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const  Padding(
-                padding: EdgeInsets.fromLTRB(0.0, 20.0, 39.0, 0.0),
-                child: Text('You have pushed the button this many times:',style:TextStyle(fontSize: 60.0))
-            ),
-
-
-            TextField(controller: _controller,
-               decoration: InputDecoration(
-                  hintText: "Type something here",
-                  labelText:"Put your first name here",
-              border: OutlineInputBorder(),
-            ),
-            ),
-
-            Padding(
-              padding: EdgeInsets.all(16.0),
-              child: Image.asset("images/algonquin.jpg", height:600, width:800),
-         ),
-
-
-            ElevatedButton( onPressed: ( ){
-              //what was typed is:
-      var input = _controller.value.text;
-
-
-      //to overwrite:
-              _controller.text = "You typed in:" + input;
-
-            }, //Lambda, or anonymous function
-                child:Image.asset("images/algonquin.jpg", height:200, width:200),  )
-
-
-          ],
+            ],
+          ),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 
-  //this runs when you click the button
-  void buttonClicked   ( ){
-
+  // Widget for images with text in the center
+  Widget buildImageStack(String imagePath, String label) {
+    return Stack(
+      alignment: Alignment.center,
+      children: [
+        CircleAvatar(
+          backgroundImage: AssetImage(imagePath),
+          radius: 50,
+        ),
+        Text(
+          label,
+          style: const TextStyle(
+            color: Colors.white,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ],
+    );
   }
 
-
+  // Widget for images with text below
+  Widget buildImageWithTextBelow(String imagePath, String label) {
+    return Column(
+      children: [
+        CircleAvatar(
+          backgroundImage: AssetImage(imagePath),
+          radius: 50,
+        ),
+        const SizedBox(height: 5), // Add spacing between the image and text
+        Text(
+          label,
+          style: const TextStyle(
+            fontWeight: FontWeight.bold,
+            fontSize: 12,
+          ),
+        ),
+      ],
+    );
+  }
 }
